7f3af2c65ac92e0e0d3fc304580dd7280e6a58e8
Sahar-DataScience <sahar.ben.mabrouk.pro@gmail.com>
2024-01-05 09:47:33 +0000

    debug lab

--- Python_Lab.py
+++ Python_Lab.py
@@ -7,14 +7,14 @@ print("Python has three numeric types: int, float, and complex")
 print("\n")
 #################################################
 
-myValue=1
+myValue_B=1
 print(myValue)
 print(type(myValue))
 print(str(myValue) +  " is of the data type" + str(type(myValue)))
 print("\n")
 #####################################################
 
-myValue=3.14
+myValue_A=3.14
 print(myValue)
 print(type(myValue))
 print(str(myValue) + " is of the data type " + str(type(myValue)))
