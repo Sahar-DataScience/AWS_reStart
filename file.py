de7ef1eb362ea49777b3faf408abffbad77f2cc6
Sahar-DataScience <sahar.ben.mabrouk.pro@gmail.com>
2024-01-05 19:22:44 +0000

    debug prime number

--- file.py
+++ file.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-prime = [1,2,3,5,7]
+prime = [1,2,3,5,7,11,13] # 13² = 169 and 17² = 289 > 250 we chose minimal prime deviders that their square is less than 250 
 
-for x in range(8,251):
+for x in range(14,251):
 	check = True
-	for d in [2,3,5,7]:
+	for d in [2,3,5,7,11,13]:
 		if x % d == 0:
 			check = False
 		
 	if check == True:
 		prime.append(x)
 print(prime)
-#print(len(prime))
+print(len(prime))
 with open("results.txt",'w') as f:
 	for a in prime:
 		f.write(str(a)+ " ")
